f25e9562c6827b4c2f2deed5a928f031e2b406aa
Katanajay <jasonmcaulfield@gmail.com>
2025-09-23 20:33:45 +0100

    add deploy.yml

--- ww2battlefieldtours/astro.config.mjs
+++ ww2battlefieldtours/astro.config.mjs
@@ -1,4 +1,6 @@
 
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://github.com/Katanajay/ww2battlefieldtours.github.io',
+});
